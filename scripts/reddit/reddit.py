7124dd35093001af6a8aa53487f6787c441eaf52
Prathamesh Walke <pvwalke09@gmail.com>
2019-11-08 02:23:03 -0500

    links object inserted in mysql

--- scripts/reddit/reddit.py
+++ scripts/reddit/reddit.py
@@ -1,12 +1,20 @@
 import requests
 import requests.auth
 import json
-import sys
+import mysql.connector
 
+test = False
 class reddit:
     def __init__(self):
         self.token = {}
         
+    def mysqlConnector(self,databaseName):
+        
+        cnx = mysql.connector.connect(user='root', password='D@hanu2009',
+                              host='localhost',
+                              database=databaseName,autocommit=True)
+        return cnx
+    
     def pythonConnectDB( databaseName, collectionName):
         import pymongo
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -37,22 +45,59 @@ class reddit:
                 cnt += 1
         return credentials
     def getAPIResponse(self, api):
-        credentials = self.getCredentials()
-        print(credentials)
-        tokenDetails = self.getAccessTokenDetails(credentials[0], credentials[1], credentials[2], credentials[3])
-        baseURL = "https://oauth.reddit.com"
-        headers = {"Authorization": "bearer " + tokenDetails['access_token'] , "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
-        response = requests.get(baseURL + api, headers=headers)
-        return response.json()
+        if(test):
+            with open("reddit_test.json", 'r') as f:
+                #response = json.load(f)
+                response = f.read()
+                #use this only when reading from json file otherwise don't use below statement
+                response = json.loads(response)
+                return response
+        else:
+            credentials = self.getCredentials()
+            print(credentials)
+            tokenDetails = self.getAccessTokenDetails(credentials[0], credentials[1], credentials[2], credentials[3])
+            baseURL = "https://oauth.reddit.com"
+            headers = {"Authorization": "bearer " + tokenDetails['access_token'] , "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+            response = requests.get(baseURL + api, headers=headers)
+            return response.json()
+        
     
         
     def commentObject_t1(self, object_t1):
-        #print("parsing and creating Comment object")
-        print(object_t1)
+        print("parsing and creating Comment object")
+        
         return object_t1
     def linkObject_t3(self, object_t3):
-        #print("parsing and creating link object")
-        print(object_t3)
+        print("t3:parsing and creating link object")
+        final = {}
+        final['id'] = object_t3['id']
+        final['subreddit_id'] = object_t3['subreddit_id'] #primary key [links]
+        final['name'] = object_t3['name'] #primary key [links]
+        final['subreddit_name_prefixed'] = object_t3['subreddit_name_prefixed']
+        final['url'] = object_t3['url']
+        final['domain'] = object_t3['domain']
+        final['total_awards_received'] = object_t3['total_awards_received']
+        final['ups'] = object_t3['ups']
+        final['score'] = object_t3['score']
+        final['pinned'] =  object_t3['pinned']
+        final['author'] = object_t3['author']
+        final['author_fullname'] = object_t3['author_fullname']
+        final['title'] = object_t3['title']
+        final['permalink'] = object_t3['permalink']
+        final['created_utc'] = object_t3['created_utc']
+        final['num_comments'] = object_t3['num_comments']
+        final['num_crossposts'] = object_t3['num_crossposts']
+        #final['downs'] = object_t3['downs']
+        #final['is_video'] = object_t3['is_video']
+        #final['view_count'] = object_t3['view_count']
+        mySql_insert_query = """INSERT INTO links VALUES 
+                           (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+        recordTuple = tuple(final.values())
+        conn =self.mysqlConnector("trenddit")
+        cur = conn.cursor()
+        cur.execute(mySql_insert_query, recordTuple)
+        conn.commit()
+        cur.close()
         return object_t3
     
     def subredditObject_t5(self, object_t5):
@@ -61,8 +106,7 @@ class reddit:
         return object_t5
 
     def getRedditJSONParsed(self, redditObject):
-        if( redditObject['kind'] == 't1'):
-            
+        if( redditObject['kind'] == 't1'):    
             return self.commentObject_t1(redditObject['data'])
         elif( redditObject['kind'] == 't3'):
             return self.linkObject_t3(redditObject['data'])
@@ -71,17 +115,8 @@ class reddit:
         
     def getRedditData(self, subReddit, outputFileName):
         output = self.getAPIResponse(subReddit)
-        for eachElem in output['data']['children'][0:1]:
+        
+        for eachElem in output['data']['children']:
             self.getRedditJSONParsed(eachElem)
-
-# =============================================================================
-#         databaseName = "Trenddit"
-#         collectionName = "reddit"
-#         collection = pythonConnectDB(databaseName, collectionName)
-#         collection.insert_one(output)
-# =============================================================================
-        #with open('data.txt', 'w') as outfile:
-            #json.dump(output, outfile, indent=4)
-
 
 reddit().getRedditData('/r/news/', 'output.json')
